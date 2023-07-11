061033ab9b904a5dfb9e3fd48f756dc204a2cbbc
Shksushartha <shksushartha@gmail.com>
2023-07-11 11:13:12 +0545

    initial commit

---
+++ encryptDecryptMVC/Controllers/HomeController.cs
@@ -0,0 +1,125 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using encryptDecryptMVC.Models;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace encryptDecryptMVC.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [HttpGet]
+    public IActionResult Encrypt()
+    {
+        return View(new encryptDecryptModel());
+    }
+
+    [HttpPost]
+    public IActionResult Encrypt(encryptDecryptModel x)
+    {
+        x.e = EncryptString(x.d);
+        return View(x);
+    }
+
+    [HttpGet]
+    public IActionResult Decrypt()
+    {
+        return View(new encryptDecryptModel());
+    }
+
+    [HttpPost]
+    public IActionResult Decrypt(encryptDecryptModel x)
+    {
+        x.d = DecryptString(x.e);
+        return View(x);
+    }
+
+
+    public static string EncryptString(string a)
+    {
+        string key = "#32!xAz)27:zXa@3";
+
+        byte[] iv = new byte[16];
+        byte[] array;
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+
+            ICryptoTransform encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter streamWriter = new StreamWriter(cryptoStream))
+                    {
+                        streamWriter.Write(a);
+                    }
+
+                    array = memoryStream.ToArray();
+                }
+            }
+        }
+
+        return Convert.ToBase64String(array);
+    }
+
+    public static string DecryptString(string a)
+    {
+        string key = "#32!xAz)27:zXa@3";
+        string cipherText;
+
+
+
+        byte[] iv = new byte[16];
+
+
+        byte[] buffer = Convert.FromBase64String(a);
+
+        using (Aes aes = Aes.Create())
+        {
+            aes.Key = Encoding.UTF8.GetBytes(key);
+            aes.IV = iv;
+            ICryptoTransform decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+
+            using (MemoryStream memoryStream = new MemoryStream(buffer))
+            {
+                using (CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader streamReader = new StreamReader(cryptoStream))
+                    {
+                        return streamReader.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
+
---
+++ encryptDecryptMVC/Models/encryptDecryptModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace encryptDecryptMVC.Models
+{
+    public class encryptDecryptModel
+    {
+        public encryptDecryptModel()
+        {
+        }
+        public string e { get; set; }
+        public string d { get; set; }
+    }
+}
+
